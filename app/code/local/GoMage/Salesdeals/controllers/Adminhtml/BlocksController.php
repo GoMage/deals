9d8c9ca6710c6d7a6bdd8f92289e30d8157ba425
Andrey Zac <az@stepcart.com>
2015-07-16 14:23:08 +0300

    fix: security

--- app/code/local/GoMage/Salesdeals/controllers/Adminhtml/BlocksController.php
+++ app/code/local/GoMage/Salesdeals/controllers/Adminhtml/BlocksController.php
@@ -1,5 +1,6 @@
 <?php
- /**
+
+/**
  * GoMage Sales and Deals Extension
  *
  * @category     Extension
@@ -10,179 +11,192 @@
  * @version      Release: 1.0
  * @since        Class available since Release 1.0
  */
+class GoMage_Salesdeals_Adminhtml_BlocksController extends Mage_Adminhtml_Controller_Action
+{
 
-class GoMage_Salesdeals_Adminhtml_BlocksController extends Mage_Adminhtml_Controller_Action{
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('promo/gomage_salesdeals/gomage_manage_deal_blocks');
+    }
 
-	
-	protected function _initAction() {
-		$this->loadLayout()
-			->_setActiveMenu('promo/gomage_salesdeals')
-			->_addBreadcrumb(Mage::helper('adminhtml')->__('Manage Deal Blocks'), Mage::helper('adminhtml')->__('Manage Deal Blocks'));
-		
-		return $this;
-	}   
- 	 	
-	public function indexAction() {
-		$this->_initAction();
-		$this->renderLayout();
-		
-	}
-	
-    public function saveAction(){
+    protected function _initAction()
+    {
+        $this->loadLayout()
+            ->_setActiveMenu('promo/gomage_salesdeals')
+            ->_addBreadcrumb(Mage::helper('adminhtml')->__('Manage Deal Blocks'), Mage::helper('adminhtml')->__('Manage Deal Blocks'));
 
-		if ($data = $this->getRequest()->getPost()) {
-			
-			try{
-			    
-			    $data = $this->_filterPostData($data); 
-			    
-			    $id = $this->getRequest()->getParam('id');
-			    
-				$model = Mage::getModel('gomage_salesdeals/block');					  			
-				
-	            $model->setData($data)->setId($id)->save();
+        return $this;
+    }
+
+    public function indexAction()
+    {
+        $this->_initAction();
+        $this->renderLayout();
+
+    }
+
+    public function saveAction()
+    {
+
+        if ($data = $this->getRequest()->getPost()) {
+
+            try {
+
+                $data = $this->_filterPostData($data);
+
+                $id = $this->getRequest()->getParam('id');
+
+                $model = Mage::getModel('gomage_salesdeals/block');
+
+                $model->setData($data)->setId($id)->save();
 
                 $connection = Mage::getSingleton('core/resource')->getConnection('write');
-	    	    $table = Mage::getSingleton('core/resource')->getTableName('gomage_salesdeals_block_entity');
-	    	    $connection->query('delete from ' . $table . ' where block_id=' . (int)$model->getId());
+                $table      = Mage::getSingleton('core/resource')->getTableName('gomage_salesdeals_block_entity');
+                $connection->query('delete from ' . $table . ' where block_id=' . (int)$model->getId());
 
-	            if (isset($data['in_block_items'])){
-	                $block_items = explode('&', $data['in_block_items']);
-	                foreach ($block_items as $block_item){
-	                    if ($block_item){
-	                        $block_item = explode('=', $block_item);
-	                        $item = Mage::getModel('gomage_salesdeals/block_item');
-	                        $item->setData(array(
-	                                        'block_id' => $model->getId(),
-	                                        'item_id'  => $block_item[0],
-	                                        'position' => $block_item[1],
-	                                    ))->save();
-	                    }
-	                }
-	            }
-	            
-	            Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('core')->__('Data successfully saved'));
-	            
-	            if ($this->getRequest()->getParam('back')) {
-	                $params = array('id' => $model->getId());
-	                if ($this->getRequest()->getParam('store')){
-	                    $params['store'] = $this->getRequest()->getParam('store'); 
-	                }
-					$this->_redirect('*/*/edit', $params);
-					return;
-				}
-				
-            }catch(Mage_Core_Exception $e){
-                
-            	Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-            	
-            	Mage::getSingleton('core/session')->setSalesdealsData($data);
-            	
-            	if($model->getId()>0){
-            	    $params = array('id' => $model->getId());
-	                if ($this->getRequest()->getParam('store')){
-	                    $params['store'] = $this->getRequest()->getParam('store'); 
-	                }
-            		$this->_redirect('*/*/edit', $params);
-            	}else{
-            		$this->_redirect('*/*/new');
-            	}
-            	return false;
-            	
-            }catch(Exception $e){
-                                            	
-            	Mage::getSingleton('adminhtml/session')->addError(Mage::helper('core')->__('Can\'t save data'));
-            	
-				Mage::getSingleton('core/session')->setSalesdealsData($data);
-			
-            	if($model->getId()>0){
-            	    $params = array('id' => $model->getId());
-	                if ($this->getRequest()->getParam('store')){
-	                    $params['store'] = $this->getRequest()->getParam('store'); 
-	                }
-            		$this->_redirect('*/*/edit', $params);
-            	}else{
-            		$this->_redirect('*/*/new');
-            	}
-            	return false;
-            	
+                if (isset($data['in_block_items'])) {
+                    $block_items = explode('&', $data['in_block_items']);
+                    foreach ($block_items as $block_item) {
+                        if ($block_item) {
+                            $block_item = explode('=', $block_item);
+                            $item       = Mage::getModel('gomage_salesdeals/block_item');
+                            $item->setData(array(
+                                    'block_id' => $model->getId(),
+                                    'item_id'  => $block_item[0],
+                                    'position' => $block_item[1],
+                                )
+                            )->save();
+                        }
+                    }
+                }
+
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('core')->__('Data successfully saved'));
+
+                if ($this->getRequest()->getParam('back')) {
+                    $params = array('id' => $model->getId());
+                    if ($this->getRequest()->getParam('store')) {
+                        $params['store'] = $this->getRequest()->getParam('store');
+                    }
+                    $this->_redirect('*/*/edit', $params);
+                    return;
+                }
+
+            } catch (Mage_Core_Exception $e) {
+
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+
+                Mage::getSingleton('core/session')->setSalesdealsData($data);
+
+                if ($model->getId() > 0) {
+                    $params = array('id' => $model->getId());
+                    if ($this->getRequest()->getParam('store')) {
+                        $params['store'] = $this->getRequest()->getParam('store');
+                    }
+                    $this->_redirect('*/*/edit', $params);
+                } else {
+                    $this->_redirect('*/*/new');
+                }
+                return false;
+
+            } catch (Exception $e) {
+
+                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('core')->__('Can\'t save data'));
+
+                Mage::getSingleton('core/session')->setSalesdealsData($data);
+
+                if ($model->getId() > 0) {
+                    $params = array('id' => $model->getId());
+                    if ($this->getRequest()->getParam('store')) {
+                        $params['store'] = $this->getRequest()->getParam('store');
+                    }
+                    $this->_redirect('*/*/edit', $params);
+                } else {
+                    $this->_redirect('*/*/new');
+                }
+                return false;
+
             }
             $this->_redirect('*/*/');
         }
-	}
-	
-	public function _filterPostData($data)
-    {                         
-        $data['categories'] = (isset($data['categories']) && is_array($data['categories']) ? implode(',', $data['categories']) : '');            
-        $data['pages'] = (isset($data['pages']) && is_array($data['pages']) ? implode(',', $data['pages']) : '');        
-        $data['customer_group_ids'] = (isset($data['customer_group_ids']) && is_array($data['customer_group_ids']) ? implode(',', $data['customer_group_ids']) : '');                
-        
+    }
+
+    public function _filterPostData($data)
+    {
+        $data['categories']         = (isset($data['categories']) && is_array($data['categories']) ? implode(',', $data['categories']) : '');
+        $data['pages']              = (isset($data['pages']) && is_array($data['pages']) ? implode(',', $data['pages']) : '');
+        $data['customer_group_ids'] = (isset($data['customer_group_ids']) && is_array($data['customer_group_ids']) ? implode(',', $data['customer_group_ids']) : '');
+
         return $data;
-    } 
-	           	
-	public function deleteAction(){
-		
-		if($id = intval($this->getRequest()->getParam('id'))){
-			
-			$this->_deleteBlocks(array($id));
-			
-		}
-		$this->_redirect('*/*/');
-	}
-	
-	public function massDeleteAction(){
-		
-		if($ids = $this->getRequest()->getParam('id')){
-			if(is_array($ids) && !empty($ids)){
-				$this->_deleteBlocks($ids);
-			}
-			
-		}
-		
-		$this->_redirect('*/*/');
-		
-	}
-	
-	protected function _deleteBlocks($ids){
-		if(is_array($ids) && !empty($ids)){
-			foreach($ids as $id){
-				
-				$block = Mage::getModel('gomage_salesdeals/block')->load($id);
-				$block->delete();
-				
-			}
-		}
-	}
-	
-	public function newAction(){
-		$this->_initAction();		
-		if($data = Mage::getSingleton('core/session')->getSalesdealsData()){
-        	Mage::register('gomage_salesdeals_block', Mage::getModel('gomage_salesdeals/block')->addData($data));
-        	Mage::getSingleton('core/session')->setSalesdealsData(null);
+    }
+
+    public function deleteAction()
+    {
+
+        if ($id = intval($this->getRequest()->getParam('id'))) {
+
+            $this->_deleteBlocks(array($id));
+
         }
-		
-		$this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit'))
-				->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit_tabs'));
-	
-		$this->renderLayout();
-		
-	}
-	
-	public function editAction(){
-		
-		$this->_initAction();
-		
-		if($id = $this->getRequest()->getParam('id')){
-        	Mage::register('gomage_salesdeals_block', Mage::getModel('gomage_salesdeals/block')->load($id));
+        $this->_redirect('*/*/');
+    }
+
+    public function massDeleteAction()
+    {
+
+        if ($ids = $this->getRequest()->getParam('id')) {
+            if (is_array($ids) && !empty($ids)) {
+                $this->_deleteBlocks($ids);
+            }
+
         }
-        
-		$this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit'))
-				->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit_tabs'));
-		
-		$this->renderLayout();
-		
-	}
-	   
+
+        $this->_redirect('*/*/');
+
+    }
+
+    protected function _deleteBlocks($ids)
+    {
+        if (is_array($ids) && !empty($ids)) {
+            foreach ($ids as $id) {
+
+                $block = Mage::getModel('gomage_salesdeals/block')->load($id);
+                $block->delete();
+
+            }
+        }
+    }
+
+    public function newAction()
+    {
+        $this->_initAction();
+        if ($data = Mage::getSingleton('core/session')->getSalesdealsData()) {
+            Mage::register('gomage_salesdeals_block', Mage::getModel('gomage_salesdeals/block')->addData($data));
+            Mage::getSingleton('core/session')->setSalesdealsData(null);
+        }
+
+        $this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit'))
+            ->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit_tabs'));
+
+        $this->renderLayout();
+
+    }
+
+    public function editAction()
+    {
+
+        $this->_initAction();
+
+        if ($id = $this->getRequest()->getParam('id')) {
+            Mage::register('gomage_salesdeals_block', Mage::getModel('gomage_salesdeals/block')->load($id));
+        }
+
+        $this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit'))
+            ->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_blocks_edit_tabs'));
+
+        $this->renderLayout();
+
+    }
+
     public function gridAction()
     {
         $this->getResponse()->setBody(
@@ -190,5 +204,5 @@ class GoMage_Salesdeals_Adminhtml_BlocksController extends Mage_Adminhtml_Contro
                 ->toHtml()
         );
     }
-    	
+
 }
--- app/code/local/GoMage/Salesdeals/controllers/Adminhtml/ItemsController.php
+++ app/code/local/GoMage/Salesdeals/controllers/Adminhtml/ItemsController.php
@@ -1,5 +1,6 @@
 <?php
- /**
+
+/**
  * GoMage Sales and Deals Extension
  *
  * @category     Extension
@@ -10,197 +11,208 @@
  * @version      Release: 1.0
  * @since        Class available since Release 1.0
  */
+class GoMage_Salesdeals_Adminhtml_ItemsController extends Mage_Adminhtml_Controller_Action
+{
 
-class GoMage_Salesdeals_Adminhtml_ItemsController extends Mage_Adminhtml_Controller_Action{
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('promo/gomage_salesdeals/gomage_manage_salesdeals');
+    }
 
-	
-	protected function _initAction() {
-		$this->loadLayout()
-			->_setActiveMenu('promo/gomage_salesdeals')
-			->_addBreadcrumb(Mage::helper('adminhtml')->__('Product Deals'), Mage::helper('adminhtml')->__('Product Deals'));
-		
-		return $this;
-	}   
- 	 	
-	public function indexAction() {
-		$this->_initAction();
-		$this->renderLayout();
-		
-	}
-	
-    public function saveAction(){
-		if ($data = $this->getRequest()->getPost()) {
-			
-			try{
-			    
-			    $data = $this->_filterPostData($data); 
-			    
-			    $id = $this->getRequest()->getParam('id');
-			    
-				$model = Mage::getModel('gomage_salesdeals/item');					  			
-				
-	            $model->setData($data)->setId($id)->save();
-	            
-	            $product = Mage::getModel('catalog/product')->load($model->getData('product_id'));
-	            $category_ids = $product->getCategoryIds();
-	            $stores_cat_ids = array(); 
-	            foreach ($product->getStoreIds() as $store_id){
-	                $store = Mage::getModel('core/store')->load($store_id);
-	                if ($store && $store->getRootCategoryId() && !in_array($store->getRootCategoryId(), $stores_cat_ids)){
-	                    $stores_cat_ids[] = $store->getRootCategoryId();
-	                }
-	                
-	            } 
-	            if (count($stores_cat_ids)){
-	                $category_ids = array_unique(array_merge($category_ids, $stores_cat_ids));
-	                $product->setCategoryIds($category_ids);
+    protected function _initAction()
+    {
+        $this->loadLayout()
+            ->_setActiveMenu('promo/gomage_salesdeals')
+            ->_addBreadcrumb(Mage::helper('adminhtml')->__('Product Deals'), Mage::helper('adminhtml')->__('Product Deals'));
+
+        return $this;
+    }
+
+    public function indexAction()
+    {
+        $this->_initAction();
+        $this->renderLayout();
+
+    }
+
+    public function saveAction()
+    {
+        if ($data = $this->getRequest()->getPost()) {
+
+            try {
+
+                $data = $this->_filterPostData($data);
+
+                $id = $this->getRequest()->getParam('id');
+
+                $model = Mage::getModel('gomage_salesdeals/item');
+
+                $model->setData($data)->setId($id)->save();
+
+                $product        = Mage::getModel('catalog/product')->load($model->getData('product_id'));
+                $category_ids   = $product->getCategoryIds();
+                $stores_cat_ids = array();
+                foreach ($product->getStoreIds() as $store_id) {
+                    $store = Mage::getModel('core/store')->load($store_id);
+                    if ($store && $store->getRootCategoryId() && !in_array($store->getRootCategoryId(), $stores_cat_ids)) {
+                        $stores_cat_ids[] = $store->getRootCategoryId();
+                    }
+
+                }
+                if (count($stores_cat_ids)) {
+                    $category_ids = array_unique(array_merge($category_ids, $stores_cat_ids));
+                    $product->setCategoryIds($category_ids);
                     $product->save();
-	            }
-	            	            
-	            Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('core')->__('Data successfully saved'));
-	            
-	            if ($this->getRequest()->getParam('back')) {
-					$this->_redirect('*/*/edit', array('id' => $model->getId()));
-					return;
-				}
-				
-            }catch(Mage_Core_Exception $e){
-                
-            	Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-            	
-            	Mage::getSingleton('core/session')->setSalesdealsData($data);
-            	
-            	if($model->getId()>0){
-            		$this->_redirect('*/*/edit', array('id' => $model->getId()));
-            	}else{
-            		$this->_redirect('*/*/new');
-            	}
-            	return false;
-            	
-            }catch(Exception $e){
-                                            	
-            	Mage::getSingleton('adminhtml/session')->addError(Mage::helper('core')->__('Can\'t save data'));
-            	
-				Mage::getSingleton('core/session')->setSalesdealsData($data);
-			
-            	if($model->getId()>0){
-            		$this->_redirect('*/*/edit', array('id' => $model->getId()));
-            	}else{
-            		$this->_redirect('*/*/new');
-            	}
-            	return false;
-            	
+                }
+
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('core')->__('Data successfully saved'));
+
+                if ($this->getRequest()->getParam('back')) {
+                    $this->_redirect('*/*/edit', array('id' => $model->getId()));
+                    return;
+                }
+
+            } catch (Mage_Core_Exception $e) {
+
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+
+                Mage::getSingleton('core/session')->setSalesdealsData($data);
+
+                if ($model->getId() > 0) {
+                    $this->_redirect('*/*/edit', array('id' => $model->getId()));
+                } else {
+                    $this->_redirect('*/*/new');
+                }
+                return false;
+
+            } catch (Exception $e) {
+
+                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('core')->__('Can\'t save data'));
+
+                Mage::getSingleton('core/session')->setSalesdealsData($data);
+
+                if ($model->getId() > 0) {
+                    $this->_redirect('*/*/edit', array('id' => $model->getId()));
+                } else {
+                    $this->_redirect('*/*/new');
+                }
+                return false;
+
             }
             $this->_redirect('*/*/');
         }
-	}
-	
-	public function _filterPostData($data)
+    }
+
+    public function _filterPostData($data)
     {
-        
-        if ($data['start_date']){
-            $data = $this->_filterDates($data, array('start_date'));
+
+        if ($data['start_date']) {
+            $data               = $this->_filterDates($data, array('start_date'));
             $data['start_time'] = implode(':', $data['start_time']);
-        }    
-        else{
+        } else {
             $data['start_date'] = null;
             $data['start_time'] = null;
-        }    
-                 
-        if ($data['end_date']){
-            $data = $this->_filterDates($data, array('end_date'));
+        }
+
+        if ($data['end_date']) {
+            $data             = $this->_filterDates($data, array('end_date'));
             $data['end_time'] = implode(':', $data['end_time']);
-        }    
-        else{
+        } else {
             $data['end_date'] = null;
             $data['end_time'] = null;
-        }             
-        
-        return $data;
-    } 
+        }
 
-	public function deleteAction(){
-		
-		if($id = intval($this->getRequest()->getParam('id'))){
-			
-			$this->_deleteItems(array($id));
-			
-		}
-		$this->_redirect('*/*/');
-	}
-	
-	public function massDeleteAction(){
-		
-		if($ids = $this->getRequest()->getParam('id')){
-			if(is_array($ids) && !empty($ids)){
-				$this->_deleteItems($ids);
-			}
-			
-		}
-		
-		$this->_redirect('*/*/');
-		
-	}
-	
-	protected function _deleteItems($ids){
-		if(is_array($ids) && !empty($ids)){
-			foreach($ids as $id){
-				
-				$item = Mage::getModel('gomage_salesdeals/item')->load($id);
-				$item->delete();
-				
-			}
-		}
-	}
-	
-	public function newAction(){
-		$this->_initAction();		
-		if($data = Mage::getSingleton('core/session')->getSalesdealsData()){
-        	Mage::register('gomage_salesdeals', Mage::getModel('gomage_salesdeals/item')->addData($data));
-        	Mage::getSingleton('core/session')->setSalesdealsData(null);
+        return $data;
+    }
+
+    public function deleteAction()
+    {
+
+        if ($id = intval($this->getRequest()->getParam('id'))) {
+
+            $this->_deleteItems(array($id));
+
         }
-		
-		$this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit'))
-				->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit_tabs'));
-	
-		$this->renderLayout();
-		
-	}
-	
-	public function editAction(){
-		
-		$this->_initAction();
-		
-		if($id = $this->getRequest()->getParam('id')){
-        	Mage::register('gomage_salesdeals', Mage::getModel('gomage_salesdeals/item')->load($id));
+        $this->_redirect('*/*/');
+    }
+
+    public function massDeleteAction()
+    {
+
+        if ($ids = $this->getRequest()->getParam('id')) {
+            if (is_array($ids) && !empty($ids)) {
+                $this->_deleteItems($ids);
+            }
+
         }
-        
-		$this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit'))
-				->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit_tabs'));
-		
-		$this->renderLayout();
-		
-	}
-	
-	public function getProductQtyAction(){
-	    $result = array();
-	    $result['qty'] = 0;
-	    
-        if($id = $this->getRequest()->getParam('product_id')){
-	     
-             $product = Mage::getModel('catalog/product')->load($id);
-             
-             if ($product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_SIMPLE){                 
-                 $result['qty'] = min(array($product->getStockItem()->getMaxSaleQty(), $product->getStockItem()->getQty()));
-             }else{
-                 $result['qty'] = $product->getStockItem()->getMaxSaleQty();
-             }                 
-            
+
+        $this->_redirect('*/*/');
+
+    }
+
+    protected function _deleteItems($ids)
+    {
+        if (is_array($ids) && !empty($ids)) {
+            foreach ($ids as $id) {
+
+                $item = Mage::getModel('gomage_salesdeals/item')->load($id);
+                $item->delete();
+
+            }
+        }
+    }
+
+    public function newAction()
+    {
+        $this->_initAction();
+        if ($data = Mage::getSingleton('core/session')->getSalesdealsData()) {
+            Mage::register('gomage_salesdeals', Mage::getModel('gomage_salesdeals/item')->addData($data));
+            Mage::getSingleton('core/session')->setSalesdealsData(null);
+        }
+
+        $this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit'))
+            ->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit_tabs'));
+
+        $this->renderLayout();
+
+    }
+
+    public function editAction()
+    {
+
+        $this->_initAction();
+
+        if ($id = $this->getRequest()->getParam('id')) {
+            Mage::register('gomage_salesdeals', Mage::getModel('gomage_salesdeals/item')->load($id));
+        }
+
+        $this->_addContent($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit'))
+            ->_addLeft($this->getLayout()->createBlock('gomage_salesdeals/adminhtml_items_edit_tabs'));
+
+        $this->renderLayout();
+
+    }
+
+    public function getProductQtyAction()
+    {
+        $result        = array();
+        $result['qty'] = 0;
+
+        if ($id = $this->getRequest()->getParam('product_id')) {
+
+            $product = Mage::getModel('catalog/product')->load($id);
+
+            if ($product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_SIMPLE) {
+                $result['qty'] = min(array($product->getStockItem()->getMaxSaleQty(), $product->getStockItem()->getQty()));
+            } else {
+                $result['qty'] = $product->getStockItem()->getMaxSaleQty();
+            }
+
         }
 
         echo Zend_Json::encode($result);
-	    
-	}
-	
-	
+
+    }
+
+
 }
