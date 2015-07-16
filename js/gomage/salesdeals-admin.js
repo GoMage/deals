d040cf12c366adc06e3de800db25957963268707
Andrey Zac <az@stepcart.com>
2015-07-16 14:27:14 +0300

    copyrights

--- js/gomage/salesdeals-admin.js
+++ js/gomage/salesdeals-admin.js
@@ -1,4 +1,16 @@
-Validation.add('gomage-salesdeals-validate-prod-qty', 'Please enter valid Deal Quantity.',		
+/**
+ * GoMage Sales and Deals Extension
+ *
+ * @category     Extension
+ * @copyright    Copyright (c) 2010-2015 GoMage (http://www.gomage.com)
+ * @author       GoMage
+ * @license      http://www.gomage.com/license-agreement/  Single domain license
+ * @terms of use http://www.gomage.com/terms-of-use
+ * @version      Release: 1.1
+ * @since        Available since Release 1.0
+ */
+
+Validation.add('gomage-salesdeals-validate-prod-qty', 'Please enter valid Deal Quantity.',
 function(v) {	
 	var result = (Validation.get('IsEmpty').test(v)
 					|| (!isNaN(parseNumber(v)) && !/^\s+$/.test(parseNumber(v))))
--- js/gomage/salesdeals.js
+++ js/gomage/salesdeals.js
@@ -1,3 +1,15 @@
+/**
+ * GoMage Sales and Deals Extension
+ *
+ * @category     Extension
+ * @copyright    Copyright (c) 2010-2015 GoMage (http://www.gomage.com)
+ * @author       GoMage
+ * @license      http://www.gomage.com/license-agreement/  Single domain license
+ * @terms of use http://www.gomage.com/terms-of-use
+ * @version      Release: 1.1
+ * @since        Available since Release 1.0
+ */
+
 var GomageSalesdealsTickers = {};
 var gsd_time_offcet = undefined;
 
